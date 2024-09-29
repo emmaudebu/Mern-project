b609f39c25caa421f6c69152c124797d019e9e18
ifeanyi chukwu <emmanueludebu@gmail.com>
2024-09-29 19:13:27 +0100

    Backend endpoints created

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -27,6 +27,26 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted " });
+  } catch (error) {
+    res.status(404).json({ success: false, message: " product not found" });
+  }
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
 app.listen(port, () => {
   connectDB();
   console.log(`Server is active at http/localhost:${port}`);
