00eae3231c13da72560149aabb2234ad0d8c1b6f
ifeanyi chukwu <emmanueludebu@gmail.com>
2024-09-29 17:16:12 +0100

    Create Products endpoint

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,15 +2,32 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./config/db.js ";
+import Product from "./models/product.model.js";
 
 const app = express();
 const port = 5000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all the fields" });
+  }
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in creating product:", error.message);
+    res.status(500).json({ sucess: false, message: "Server Error" });
+  }
 });
 
 app.listen(port, () => {
   connectDB();
-  console.log(`Server is active at http/local:${port}`);
+  console.log(`Server is active at http/localhost:${port}`);
 });
